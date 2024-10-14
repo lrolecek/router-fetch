b35039973eee1c1b0dfdaa2c2c1e7fb25e684153
Luděk Roleček <ludek@rolecek.cz>
2024-10-14 22:07:42 +0200

    Editace knihy

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,13 +1,172 @@
-import { useParams } from "react-router-dom";
+import { useParams, useOutletContext } from "react-router-dom";
+import {useState, useEffect} from 'react';
+
+
+function BookDetail({book, onEdit}) {
+  return (
+    <>
+      <h3 className="text-xl font-bold mb-3">{book.title}</h3>
+      <p><strong>Author:</strong> {book.author}</p>
+      <p><strong>Year:</strong> {book.year}</p>
+      <p><strong>Rating:</strong> {'⭐'.repeat(book.rating)}</p>
+      <p>{book.summary}</p>
+      <button className="mt-4 bg-sky-600 text-white p-3" onClick={onEdit}>Editovat knihu</button>
+    </>
+  )
+}
+
+function BookEdit({book, onSave, onCancel}) {
+
+  const [form, setForm] = useState(book)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // validace formulare
+    // ... ted preskocime
+
+    onSave(form)
+  }
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+
+        <div className="mt-2">
+          <label className="block font-bold">Title</label>
+          <input
+            className="border border-slate-500 p-1 w-64" type="text"
+            name="title"
+            value={form.title}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mt-2">
+          <label className="block font-bold">Author</label>
+          <input
+            className="border border-slate-500 p-1 w-64" type="text"
+            name="author"
+            value={form.author}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mt-2">
+          <label className="block font-bold">Year</label>
+          <input
+            className="border border-slate-500 p-1 w-64" type="text"
+            name="year"
+            value={form.year}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mt-2">
+          <label className="block font-bold">Rating</label>
+          <input
+            className="border border-slate-500 p-1 w-64" type="text"
+            name="rating"
+            value={form.rating}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mt-2">
+          <label className="block font-bold">Summary</label>
+          <textarea
+            className="border border-slate-500 p-1 w-64" type="text" rows="5"
+            name="summary"
+            value={form.summary}
+            onChange={handleChange}
+          />
+        </div>
+
+
+        <div className="flex gap-2 mt-4">
+          <button type="submit" className="mt-4 bg-sky-600 text-white p-3">Uložit</button>
+
+          <button className="mt-4 bg-slate-600 text-white p-3" onClick={onCancel}>Zrušit</button>
+        </div>
+
+      </form>
+    </>
+  )
+}
+
+
+
+
+
+
+
 
 export function Book() {
 
   const {bookId} = useParams()
+  const {onBookUpdate} = useOutletContext()
+
+  const [book, setBook] = useState(null)
+  const [isEdited, setIsEdited] = useState(false)
+
+  useEffect(
+    () => {
+      async function getBook() {
+        const response = await fetch(`http://localhost:3000/books/${bookId}`)
+        const data = await response.json()
+        setBook(data)
+      }
+
+      getBook()
+    },
+    [bookId]
+  )
+
+
+  const saveBook = async (updatedBook) => {
+    const response = await fetch(`http://localhost:3000/books/${book.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedBook)
+    })
+
+    if (response.ok) {
+      setBook(updatedBook)
+      setIsEdited(false)
+      onBookUpdate()
+    } else {
+      console.log('chyba pri ukladani', response.statusText)
+    }
+
+  }
 
   return (
     <div>
-      <h3 className="text-xl font-bold mb-3">Kniha: {bookId}</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi repudiandae delectus quia eveniet, labore corrupti distinctio libero sit obcaecati maiores reiciendis unde repellat consequuntur ab vel voluptas? Laborum quam voluptates assumenda nemo cumque? Vitae, suscipit facere incidunt adipisci eaque nostrum a eum modi ratione iure quisquam voluptate saepe dolorem perspiciatis!</p>
+      {
+        book === null
+        ? <p>Načítám data...</p>
+        : (
+            isEdited
+            ? <BookEdit
+                book={book}
+                onSave={saveBook}
+                onCancel={() => {setIsEdited(false)}}
+              />
+            : <BookDetail
+                book={book}
+                onEdit={() => {setIsEdited(true)}}
+              />
+          )
+      }
     </div>
   );
 }
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,21 +1,64 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 export function Books() {
+
+  const [books, setBooks] = useState(null)
+  const [error, setError] = useState('')
+
+
+  async function getBooks() {
+    try {
+      const response = await fetch('http://localhost:3000/books')
+      if (response.ok) {
+        const data = await response.json()
+        setBooks(data)
+      } else {
+        setError(`${response.status} ${response.statusText}`)
+      }
+    } catch (err) {
+      setError(err.message)
+    }
+  }
+
+
+  useEffect(
+    () => {
+      getBooks()
+    },
+    []
+  )
+
+  const handleBookUpdate = () => {
+    getBooks()
+  }
+
   return (
     <div className="p-5">
       <h2 className="text-2xl font-bold mb-3">Knihy</h2>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quidem officia? Officia officiis aliquam optio. Odit suscipit repellendus minima minus.</p>
 
+      {error && <p className="text-red-600 mt-5"><strong>CHYBA:</strong><br />{error}</p>}
+
       <div className="flex gap-6 mt-6">
-        <nav className="bg-sky-100 p-3 w-48 shrink-0">
-          <ul>
-            <li>Kniha 1</li>
-            <li>Kniha 3</li>
-            <li>Kniha 4</li>
-          </ul>
+        <nav className="bg-sky-100 p-3 w-64 shrink-0">
+          {
+            books === null
+            ? <p>Načítám data...</p>
+            : <ul>
+                {books.map(book => (
+                  <li key={book.id} className="border border-b-sky-700">
+                    <Link to={book.id} className="block p-2">
+                      <strong>{book.title}</strong><br/>
+                      <span className="text-sm text-slate-400">{book.author}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+          }
         </nav>
 
-        <Outlet />
+        <Outlet context={{onBookUpdate: handleBookUpdate}} />
       </div>
     </div>
   );
